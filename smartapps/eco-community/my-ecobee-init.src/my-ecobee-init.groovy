5866fbb219ac535101aa87473fef28686555be26
Sean Schneyer <sean@linuxbox.org>
2015-12-19 00:53:45 -0600

    Minor adjustment to header file

--- smartapps/eco-community/my-ecobee-init.src/my-ecobee-init.groovy
+++ smartapps/eco-community/my-ecobee-init.src/my-ecobee-init.groovy
@@ -1,5 +1,5 @@
 /**
- *	MyecobeeInit (Service Manager)
+ *	My Ecobee Init (Service Manager)
  *  Copyright 2015 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
