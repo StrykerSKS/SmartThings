67c8311c390183ef63432306444a6e072b6bf702
snakedog116 <snakedog116@users.noreply.github.com>
2015-12-18 17:29:43 -0700

    Update ecobeeremotesensorsinit.groovy
    Updated to make work with git hub

--- smartapps/eco-community/ecobeeremotesensorsinit.src/ecobeeremotesensorsinit.groovy
+++ smartapps/eco-community/ecobeeremotesensorsinit.src/ecobeeremotesensorsinit.groovy
@@ -15,7 +15,7 @@
  *
  */
 definition(
-	name: "ecobee3RemoteSensorsInit",
+	name: "ecobeeremotesensorsinit",
 	namespace: "eco-community",
 	author: "Yves Racine",
 	description: "Create individual ST sensors for all selected ecobee3's remote sensors and update them on a regular basis (interval chosen by the user).",
